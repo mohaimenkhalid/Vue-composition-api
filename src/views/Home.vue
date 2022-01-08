608c5444d516863be229001dc0fcf96e610cdfc0
Abdul Mohaimen <mohaimen707@gmail.com>
2022-01-08 22:42:17 +0600

    composable logic implement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,32 +10,14 @@
 </template>
 
 <script>
-import {ref, reactive, computed, watch, watchEffect} from 'vue'
 import PostList from "@/components/PostList";
+import getPosts from "@/composables/getPosts";
 export default {
   name: 'Home',
   components: { PostList },
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-
-    const load = async () => {
-      try {
-        let data = await fetch('http://localhost:3000/posts')
-        if (!data.ok) {
-          throw Error('No data available.')
-        }
-        posts.value = await data.json();
-
-      } catch (err) {
-        error.value = err.message
-        console.log(err.message)
-        console.log(err.value)
-      }
-    }
+    const {load, error, posts} = getPosts();
     load()
-
-
     return {
       posts,
       error
